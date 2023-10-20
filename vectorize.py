e128be33f169aa5b74cec761a3ea738567a6282b
dark-knight37 <cristinawise208@gmail.com>
2023-10-20 04:20:07 +0100

    save while vectorize

--- vectorize.py
+++ vectorize.py
@@ -13,6 +13,7 @@ def vectorize(metadata):
 def create_parquet(parquet_file, start, end):
     df = pd.read_parquet(parquet_file, engine='pyarrow')
     new_df = df.iloc[start:end+1]
+    new_df[['fileIdentifier', 'metadata']].to_csv('database.txt')
     new_df['vector'] = new_df['metadata'].apply(vectorize)
     new_df.to_parquet(f'parquet/thingiverse-{start}-{end}.parquet', engine='pyarrow')
 
