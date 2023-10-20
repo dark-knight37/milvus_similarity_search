ef7c83530ecea2c4051cead7da42c70487f3f64a
dark-knight37 <cristinawise208@gmail.com>
2023-10-20 03:17:58 +0100

    change object download

--- download_object.py
+++ download_object.py
@@ -5,10 +5,11 @@ df = pd.read_parquet('objaverse/thingiverse/thingiverse.parquet', engine='pyarro
 # df = pd.read_parquet('objaverse/sketchfab/sketchfab.parquet', engine='pyarrow')
 # df = pd.read_parquet('objaverse/smithsonian/smithsonian.parquet', engine='pyarrow')
 # df = pd.read_parquet('objaverse/thingiverse/thingiverse.parquet', engine='pyarrow')
-sampled_df = df[101:110]
-oxl.download_objects(sampled_df)
+sampled_df = df[1001:1010]
+oxl.download_objects(sampled_df, download_dir='objects')
 
 # save metadata into a file
 with open('objaverse.txt', 'w', encoding='UTF-8') as f:
     df_string = sampled_df.to_string(header=True, index=False)
     f.write(df_string)
+    
